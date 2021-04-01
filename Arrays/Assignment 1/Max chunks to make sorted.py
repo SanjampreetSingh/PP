56e855aa1bb719c4d59ae9eab053ecdb2ea629d1
Your Name <you@example.com>
2021-04-01 19:24:09 +0530

    Max chunks to make sorted

---
+++ Arrays/Assignment 1/Max chunks to make sorted.py
@@ -0,0 +1,37 @@
+from typing import List
+
+
+class Solution:
+    def maxChunksToSorted(self, arr: List[int]) -> int:
+        p_max = -1e20
+        ans = 0
+        for i in range(0, len(arr)):
+            p_max = max(p_max, arr[i])
+            if (p_max == i):
+                ans += 1
+        return ans
+
+
+"""
+Given an array arr that is a permutation of [0, 1, ..., arr.length - 1], we split the array into some number of "chunks" (partitions), and individually sort each chunk.  After concatenating them, the result equals the sorted array.
+
+What is the most number of chunks we could have made?
+
+Example 1:
+Input: arr = [4,3,2,1,0]
+Output: 1
+Explanation:
+Splitting into two or more chunks will not return the required result.
+For example, splitting into [4, 3], [2, 1, 0] will result in [3, 4, 0, 1, 2], which isn't sorted.
+
+Example 2:
+Input: arr = [1,0,2,3,4]
+Output: 4
+Explanation:
+We can split into two chunks, such as [1, 0], [2, 3, 4].
+However, splitting into [1, 0], [2], [3], [4] is the highest number of chunks possible.
+
+Note:
+arr will have length in range [1, 10].
+arr[i] will be a permutation of [0, 1, ..., arr.length - 1].
+"""
