2584ee285ce0c68103c060d344fdd5b2d822bf40
Sanjampreet Singh <sanjam@gmail.com>
2021-08-24 21:25:15 +0530

    Suffix max and sum

---
+++ Learnings/01. Arrays and Dynamic Arrays/03. Suffix Max/solution.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def suffix_max(self, arr_input: List) -> List:
+        len_arr = len(arr_input)
+        suffix_arr = [0]*len_arr
+        suffix_arr[len_arr-1] = arr_input[len_arr-1]
+
+        for i in range(len_arr-2, -1, -1):
+            suffix_arr[i] = max(suffix_arr[i+1], arr_input[i])
+
+        return suffix_arr
+
+
+if __name__ == "__main__":
+    arr_input = [21, 3, 55, 613, 7, 31, 63, 78, 53]
+    obj = Solution()
+    print(obj.suffix_max(arr_input))
