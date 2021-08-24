1195bf0c56b716166d803bc1b1604262298e6451
Sanjampreet Singh <sanjam@gmail.com>
2021-08-24 21:18:43 +0530

    Prefix Array

---
+++ Learnings/01. Arrays and Dynamic Arrays/02. Prefix Sum/solution.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def prefix_sum(self, arr_input: List) -> List:
+        len_arr = len(arr_input)
+        prefix_arr = [0]*len_arr
+        prefix_arr[0] = arr_input[0]
+
+        for i in range(1, len_arr):
+            prefix_arr[i] = prefix_arr[i-1] + arr_input[i]
+
+        return prefix_arr
+
+
+if __name__ == "__main__":
+    arr_input = [21, 3, 55, 63, 125, 63, 7, 31, 63, 78, 631, 53]
+    obj = Solution()
+    print(obj.prefix_sum(arr_input))
