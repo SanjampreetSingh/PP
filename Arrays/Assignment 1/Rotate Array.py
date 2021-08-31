2c9dced690a2899c6be379879dda31b4ed5e1414
Sanjampreet Singh <sanjam@gmail.com>
2021-09-01 00:46:02 +0530

    Rotate Array

--- Arrays/Assignment 1/Rotate Array.py
+++ Arrays/Assignment 1/Rotate Array.py
@@ -1,15 +1,4 @@
-from typing import List
 
-
-class Solution:
-    def rotate(self, nums: List[int], k: int) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        n = len(nums)
-        nums[0:(n-k)] = reversed(nums[0:(n-k)])
-        nums[(n-k):n] = reversed(nums[(n-k):n])
-        nums[0:n] = reversed(nums[0:n])
 
 
 """
