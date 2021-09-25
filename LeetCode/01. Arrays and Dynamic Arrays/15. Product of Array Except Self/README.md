f04bd1e8f128b496b3d67e9f5f4441fa35aad394
Sanjampreet Singh <sanjam@gmail.com>
2021-09-26 00:34:20 +0530

    Product of Array Except Self

---
+++ LeetCode/01. Arrays and Dynamic Arrays/15. Product of Array Except Self/README.md
@@ -0,0 +1,11 @@
+# [238] Product of Array Except Self
+
+[Link](https://leetcode.com/problems/product-of-array-except-self/)
+
+**Statistics**
+
+1. Time complexity - O(n)
+2. Space complexity - O(1)
+
+**Result**:  
+![Result image](https://github.com/SanjampreetSingh/PP/blob/master/LeetCode/01.%20Arrays%20and%20Dynamic%20Arrays/15.%20Product%20of%20Array%20Except%20Self/image.jpg)
--- LeetCode/01. Arrays and Dynamic Arrays/15. Product of Array Except Self/solution.py
+++ LeetCode/01. Arrays and Dynamic Arrays/15. Product of Array Except Self/solution.py
@@ -7,17 +7,22 @@ class Solution:
         s_prod = [0]*n
         s_prod[n-1] = nums[n-1]
 
-        for i in range(n-2, 0, -1):
+        for i in range(n-2, -1, -1):
             s_prod[i] = s_prod[i+1] * nums[i]
 
         x = nums[0]
         p_prod = 1
+        s_prod[0] = s_prod[1]
 
-        nums[0] = s_prod[1]
         for i in range(1, n-1):
             p_prod *= x
             x = nums[i]
-            nums[i] = p_prod*s_prod[i+1]
-        nums[n-1] = p_prod * x
+            s_prod[i] = p_prod*s_prod[i+1]
 
-        return nums
+        s_prod[n-1] = p_prod * x
+        return s_prod
+
+
+if __name__ == "__main__":
+    obj = Solution()
+    print(obj.productExceptSelf([4, 3, 2, 1, 2]))
