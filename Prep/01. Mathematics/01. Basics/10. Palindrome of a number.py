5f438d2f39370c3f666fbaa2eef244cb38e4282d
Sanjampreet Singh <sanjam531@gmail.com>
2022-10-25 20:32:55 +0530

    Palindrome of a number

---
+++ Prep/01. Mathematics/01. Basics/10. Palindrome of a number.py
@@ -0,0 +1,16 @@
+class Solution:
+    def palindrome(self, n: int) -> bool:
+        temp = n
+        reverse = 0
+
+        while temp != 0:
+            last_digit = temp % 10
+            reverse = reverse * 10 + last_digit
+            temp = temp // 10
+
+        return reverse == n
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.palindrome(12321))
