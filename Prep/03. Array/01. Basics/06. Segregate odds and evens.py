b17e1d2b92b227dfb22bb93895b812c9b128d9ee
Sanjampreet Singh <sanjam531@gmail.com>
2022-10-31 20:38:41 +0530

    List basics

---
+++ Prep/03. Array/01. Basics/06. Segregate odds and evens.py
@@ -0,0 +1,18 @@
+class Solution:
+    def odd_even(self, lst: list) -> list:
+        odd = []
+        even = []
+        for i in lst:
+            if i % 2 == 0:
+                even.append(i)
+            else:
+                odd.append(i)
+
+        return odd, even
+
+
+if __name__ == "__main__":
+    s = Solution()
+    odd, even = s.odd_even([1, 2, 3, 4, 5])
+    print("Odd are: " + str(odd))
+    print("Even are: " + str(even))
---
+++ Prep/03. Array/01. Basics/07. Get smaller element.py
@@ -0,0 +1,9 @@
+class Solution:
+    def smaller_elements(self, lst: list, x: int) -> list:
+        return [i for i in lst if i < x]
+
+
+
+class Solution:
+    s = Solution()
+    print(s.smaller_elements([1,2,3,4,5,5], 4))
