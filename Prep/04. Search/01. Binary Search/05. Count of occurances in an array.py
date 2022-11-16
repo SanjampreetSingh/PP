71b8935eb8450596555fbcfd31b6285d2cea1f45
Sanjampreet Singh <sanjam531@gmail.com>
2022-11-16 23:11:28 +0530

    count frequency in array using binary search

---
+++ Prep/04. Search/01. Binary Search/05. Count of occurances in an array.py
@@ -0,0 +1,47 @@
+class Solution:
+    def first_occurrences(self, arr: list, x: int) -> int:
+        low = 0
+        high = len(arr) - 1
+        while low <= high:
+            mid = (low + high) // 2
+            if arr[mid] < x:
+                low = mid + 1
+            elif arr[mid] > x:
+                high = mid - 1
+            else:
+                if mid == 0 or arr[mid] != arr[mid - 1]:
+                    return mid
+                else:
+                    high = mid - 1
+
+        return -1
+
+    def last_occurrences(self, arr: list, x: int) -> int:
+        low = 0
+        high = len(arr) - 1
+        while low <= high:
+            mid = (low + high) // 2
+            if arr[mid] < x:
+                low = mid + 1
+            elif arr[mid] > x:
+                high = mid - 1
+            else:
+                if mid == len(arr) - 1 or arr[mid] != arr[mid + 1]:
+                    return mid
+                else:
+                    low = mid + 1
+
+        return -1
+
+    def count_occurrences(self, arr: list, x: int) -> int:
+        first = self.first_occurrences(arr, x)
+
+        if first == -1:
+            return -1
+        else:
+            return self.last_occurrences(arr, x) - first + 1
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.count_occurrences([10,20,20,20,30], 20))
