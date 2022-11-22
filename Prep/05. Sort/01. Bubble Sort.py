e10cedd6f0bd7f3517003848d4d55e6be40ae9e1
Sanjampreet Singh <sanjam531@gmail.com>
2022-11-22 23:29:01 +0530

    bubble_sort_optimized

--- Prep/05. Sort/01. Bubble Sort.py
+++ Prep/05. Sort/01. Bubble Sort.py
@@ -7,7 +7,20 @@ class Solution:
 
         return arr
 
+    def bubble_sort_optimized(self, arr: list) -> list:
+        for i in range(len(arr) - 1):
+            swapped = False
+            for j in range(i, len(arr) - i - 1):
+                if arr[j] > arr[j + 1]:
+                    arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                    swapped = True
+            if swapped == False:
+                return arr
+
+        return arr
+
 
 if __name__ == "__main__":
     s = Solution()
     print(s.bubble_sort([2, 10, 8, 6]))
+    print(s.bubble_sort_optimized([2, 10, 8, 6]))
